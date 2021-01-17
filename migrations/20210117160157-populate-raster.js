51b8a0b816b689db0b44b6a8a1d17dddd7a95b59
Andreas Hocevar <andreas.hocevar@gmail.com>
2021-01-18 00:26:30 +0100

    Remove unused variables

--- migrations/20210117160157-populate-raster.js
+++ migrations/20210117160157-populate-raster.js
@@ -3,8 +3,8 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const raster = [];
-    for (let lat = 42.25, c = 's', cc = 'W'; lat >= 34.75; c = c == 'z' ? 'A' : String.fromCharCode(c.charCodeAt(0) + 1), lat -= 0.25) {
-      for (let lon = 19.25, r = 0, rr = 45; lon <= 31.25; ++r, lon += 0.25) {
+    for (let lat = 42.25, c = 's'; lat >= 34.75; c = c == 'z' ? 'A' : String.fromCharCode(c.charCodeAt(0) + 1), lat -= 0.25) {
+      for (let lon = 19.25, r = 0; lon <= 31.25; ++r, lon += 0.25) {
         raster.push({
           Bezeichnung: `${c}${r}`,
           LinksUntenOst: lon,
